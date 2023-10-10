c8284797bde0ad22720579fbe4a1f0598f0fb389
niteshjain96 <140519766+niteshjain96@users.noreply.github.com>
2023-10-10 20:40:47 +0530

    Add files via upload

---
+++ Strings/StringCompression.java
@@ -0,0 +1,49 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+	public static String compression1(String str){
+		String s="";
+		for(int i=0;i<str.length()-1;i++){
+		    if(str.charAt(i)!=str.charAt(i+1)){
+		        s += str.charAt(i);
+		    }
+		}
+		s+=str.charAt(str.length()-1);
+		return s;
+	}
+
+	public static String compression2(String str){
+		String ans="";
+		int c=1;
+		for(int i=0;i<str.length()-1;i++){
+		    if(str.charAt(i)==str.charAt(i+1))
+		    {
+		        c++;
+		    }
+		    else
+		    {
+		        ans+=str.charAt(i);
+		        if(c>1)
+		        {
+		            ans+=c;
+		            c=1;
+		        }
+		    }
+		}
+		ans+=str.charAt(str.length()-1);
+		    if(c>1)
+		    {
+		        ans+=c;
+		    }
+		return ans;
+	}
+	public static void main(String[] args) {
+		Scanner scn = new Scanner(System.in);
+		String str = scn.next();
+		System.out.println(compression1(str));
+		System.out.println(compression2(str));
+	}
+
+}
